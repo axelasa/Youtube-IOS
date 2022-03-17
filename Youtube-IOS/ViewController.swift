b1dd58721393e0ff3b5c9a1d5e7f4baad13ecfa0
axelasa <asaaxel82@gmail.com>
2022-03-17 15:09:51 +0300

    code to test bitrise

--- Youtube-IOS/ViewController.swift
+++ Youtube-IOS/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         var i = 0
+        
+        i+= 1
+        print(i)
     }
 
 
