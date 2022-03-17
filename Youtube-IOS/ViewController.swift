20dac42f8e3685bf0bc673574e16122ec82b47e4
axelasa <asaaxel82@gmail.com>
2022-03-17 14:26:24 +0300

    Initial xcode project

---
+++ Youtube-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Youtube-IOS
+//
+//  Created by LCT AFRICA on 17/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
